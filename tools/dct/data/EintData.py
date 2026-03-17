564097bfd7ebc48b388d67a6c1913883bb91fb91
Kumar Saptam <45693173+Sapython@users.noreply.github.com>
2026-03-17 15:06:02 +0530

    Remove unnecessary list() around dict.keys()

--- tools/dct/data/EintData.py
+++ tools/dct/data/EintData.py
@@ -77,7 +77,7 @@ class EintData(object):
     def get_modeName(gpio_num, mode_idx):
         key = "gpio%s" % (gpio_num)
 
-        if key in list(EintData._mode_map.keys()):
+        if key in EintData._mode_map.keys():
             list = EintData._mode_map[key]
             if mode_idx < len(list) and mode_idx >= 0:
                 return list[mode_idx]
